153bac93a015af5ceda46a4a02a4287fcea19bc8
Jose Vega <josevega234@gmail.com>
2021-06-21 02:17:07 +0200

    scafolding

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import controllers.MainViewController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/MainView.fxml"));
+        MainViewController mvController = new MainViewController();
+        loader.setController(mvController);
+        VBox vBox = loader.load();
+        Scene mainScene = new Scene(vBox, 1200, 700);
+        primaryStage.setTitle("TornaBOX");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+}
