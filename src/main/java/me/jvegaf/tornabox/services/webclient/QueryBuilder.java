4df26dfadae2a06af9a97dd3e7d36112945f5a59
Jose Vega <josevega234@gmail.com>
2021-12-19 05:01:56 +0100

    refactor querbuilder

--- src/main/java/me/jvegaf/tornabox/services/webclient/QueryBuilder.java
+++ src/main/java/me/jvegaf/tornabox/services/webclient/QueryBuilder.java
@@ -8,9 +8,7 @@ public class QueryBuilder {
     public static QueryDTO build(String[] strArgs) {
         //        joeski+un+congo
         var elements = sanitize(strArgs);
-        QueryDTO qvo = new QueryDTO(elements);
-        System.out.println("query builded: " + qvo.Value());
-        return qvo;
+        return new QueryDTO(elements);
     }
 
     private static List<String> sanitize(String[] qArgs) {
